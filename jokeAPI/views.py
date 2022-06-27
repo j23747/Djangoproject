c48b238a514207392c3f7645870e2e56abe90768
j23747 <josphatmwania237@gmail.com>
2022-06-27 13:49:43 +0300

    ft-add-template-View

--- jokeAPI/views.py
+++ jokeAPI/views.py
@@ -5,6 +5,15 @@ import requests
 # Fetch API data & display function from the joke API
 
 
-def  index(request):
-    response=request.get('https://official-joke-api.appspot.com/jokes/random').json()
-    return render(request, 'index.html', {'response' :response})
+# def  index(request):
+#     response=request.get('https://official-joke-api.appspot.com/jokes/random').json()
+#     return render(request, 'index.html', {'response' :response})
+
+
+def getJoke(TemplateView):
+    teplate_name = 'jokes.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        pass
+
+
