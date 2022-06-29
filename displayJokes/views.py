b8f169dad5166f995efc245628fc9235c398b34a
j23747 <josphatmwania237@gmail.com>
2022-06-29 22:06:08 +0300

    Deleted unsed app

--- displayJokes/views.py
+++ displayJokes/views.py
@@ -5,20 +5,15 @@ import requests
 
 # get the data from rest API
 def jokes(request):
-    # response = requests.get('https://dad-jokes.p.rapidapi.com/random/joke')
-    # response = requests.get('https://jsonplaceholder.typicode.com/users')
-    response = requests.get('https://dad-jokes.p.rapidapi.com/random/joke')
     
-
-    # response = requests.get('https://api.chucknorris.io/jokes/random')
+    response = requests.get('https://api.chucknorris.io/jokes/random')
 
     #Converting the response data into json
 
     jokes = response.json()
 
     return render(request, 'display_jokes.html', {'jokes': jokes})
-    # print(joke)
-    pass
+    
 
 
 
