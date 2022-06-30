6eb0b18426dc094a220b3208795aae3cc49c6ccf
j23747 <josphatmwania237@gmail.com>
2022-06-30 12:04:34 +0300

    Create alternate branch

---
+++ jokeAPI/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register JokeAPI in the admin
+
+# from .models import jokeAPI
+
+# admin.site.register(jokeAPI)
+
+# admin.site.register(display_jokes)
---
+++ jokeAPI/service.py
@@ -0,0 +1,9 @@
+import os
+import requests
+
+# def GetJokes():
+#     url = 'https://api.chucknorris.io/jokes/random'
+#     r = requests.get(url)
+#     joke =  r.json()
+
+#     return joke
